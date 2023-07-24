4cf241c4d8f89094b727aea7bf1898a56c2fba65
Danny Byrne <danny.byrne.dev@gmail.com>
2023-07-23 19:22:33 -0700

    implement image tag parsing

--- src/app/components/DocReader.js
+++ src/app/components/DocReader.js
@@ -1,7 +1,18 @@
 import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+
+const strangeRippleImageIdAsKeyPathAsValues = {
+  carlJung: { id: "carlJung", path: "https://i.imgur.com/4QZKX0M.png" },
+  carlJungBookImage: {
+    id: "carlBookImage",
+    path: "https://i.imgur.com/4QZKX0M.png",
+  },
+  ayaUFOs: { id: "ayaUFOs", path: "./images/ayaUFOs.png" },
+};
 
 const DocxReader = () => {
   const [htmlContent, setHtmlContent] = useState("");
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
     console.log({ htmlContent });
@@ -12,13 +23,32 @@ const DocxReader = () => {
       .then((response) => response.json())
       // .then((data) => console.log({ data }))
       .then((data) => {
-        const doc = new DOMParser().parseFromString(data.html, "text/xml");
-        const innerHTML = doc.firstChild.innerHTML;
-        const html = data.html;
+        // const doc = new DOMParser().parseFromString(data.html, "text/xml");
+        // const innerHTML = doc.firstChild.innerHTML;
+
+        let html = data.html;
+
+        // Replace the unique markers with placeholders for the images
+        html = html.replace(
+          `<${strangeRippleImageIdAsKeyPathAsValues.ayaUFOs.id}>`,
+          `<div id="${strangeRippleImageIdAsKeyPathAsValues.ayaUFOs.id}"></div>`
+        );
+
         setHtmlContent(html);
       })
       .catch((err) => console.error(err));
   }, []);
+
+  useEffect(() => {
+    if (htmlContent) {
+      ReactDOM.render(
+        <img src="./images/ayaUFOs.png" />,
+        document.getElementById("ayaUFOs")
+      );
+
+      // setMounted(true);
+    }
+  }, [htmlContent]);
 
   return (
     <div className="test class">
