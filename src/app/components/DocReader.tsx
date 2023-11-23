e4fd05f5a05ba472eff83c40841390f4357b5ba8
Danny Byrne <danny.byrne.dev@gmail.com>
2023-11-23 14:53:44 -0800

    parsing IDs

--- src/app/components/DocReader.tsx
+++ src/app/components/DocReader.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import parse, { DOMNode } from "html-react-parser";
+import parse, { DOMNode, domToReact } from "html-react-parser";
 
 type ImageMap = {
   [key: string]: { id: string; path: string };
@@ -70,10 +70,51 @@ const DocxReader: React.FC = () => {
     }
   };
 
-  //TODONEXT: figure out
+  //TODONEXT: figure out parse with obj.transform as second arg
 
   // const content = parse(htmlContent, { transform: replaceDOM });
-  const content = parse(htmlContent);
+
+  // const options = {
+  //   replace({ attribs, children }: { attribs: any; children: any[] }) {
+  //     if (!attribs) {
+  //       return;
+  //     }
+
+  //     if (attribs.id) {
+  //       // return <h1 style={{ fontSize: 42 }}>{domToReact(children, options)}</h1>;
+  //       console.log('id: ', attribs.id);
+  //     }
+
+  //     // if (attribs.id === 'main') {
+  //     //   return <h1 style={{ fontSize: 42 }}>{domToReact(children, options)}</h1>;
+  //     // }
+
+  //     // if (attribs.class === 'prettify') {
+  //     //   return (
+  //     //     <span style={{ color: 'hotpink' }}>
+  //     //       {domToReact(children, options)}
+  //     //     </span>
+  //     //   );
+  //     // }
+  //   },
+  // };
+
+  const options = {
+    replace(
+      domNode: DOMNode
+    ): false | void | object | Element | null | undefined {
+      if (
+        domNode.type === "tag" &&
+        "attribs" in domNode &&
+        domNode.attribs &&
+        domNode.attribs.id
+      ) {
+        console.log("id: ", domNode.attribs.id);
+      }
+    },
+  };
+
+  const content = parse(htmlContent, options);
   console.log({ content });
 
   return (
