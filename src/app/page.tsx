704ac33a0e3bd58cf825cf75e3f43c7433e63601
Danny Byrne <danny.byrne.dev@gmail.com>
2024-01-12 11:03:10 -0800

    add media queries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,27 +4,45 @@ import dynamic from "next/dynamic";
 import styled from "styled-components";
 import { colors } from "./components/utils";
 
+const BREAKPOINTS = {
+  mobile: "768px",
+  tablet: "1024px",
+  desktop: "1280px",
+};
+
 const StyledPage = styled.div`
   width: 100vw;
   display: block;
   overflow: auto;
   color: ${colors.darkWhite};
-
-  // display: flex;
-  // flex-direction: column;
-  // align-items: start;
-  // justify-content: center;
   background-color: ${colors.darkGrey};
 
-  padding-left: 550px;
-  padding-right: 550px;
+  padding-left: 10%;
+  padding-right: 10%;
+
+  @media (min-width: ${BREAKPOINTS.mobile}) {
+    padding-left: 20%;
+    padding-right: 20%;
+  }
+
+  @media (min-width: ${BREAKPOINTS.tablet}) {
+    padding-left: 25%;
+    padding-right: 25%;
+  }
 
   h1 {
-    font-size: 3rem;
+    font-size: 2rem;
+
+    @media (min-width: ${BREAKPOINTS.mobile}) {
+      font-size: 3rem;
+    }
   }
 
   h2 {
-    font-size: 2rem;
+    font-size: 1.3rem;
+    @media (min-width: 768px) {
+      font-size: 2rem;
+    }
   }
 
   a {
