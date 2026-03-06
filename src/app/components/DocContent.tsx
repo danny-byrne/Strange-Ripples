c9709d0e7fd3cb4eee232d7573934848b6893696
danny-byrne <danny.byrne.dev@gmail.com>
2026-03-05 19:33:22 -0800

    revert DocContent change

--- src/app/components/DocContent.tsx
+++ src/app/components/DocContent.tsx
@@ -1,27 +1,13 @@
 "use client";
 
 import parse from "html-react-parser";
-import { useEffect, useState, useTransition } from "react";
 import { parserOptions } from "./utils";
 import About from "./About";
 
 export default function DocContent({ html }: { html: string }) {
-  const [content, setContent] = useState<React.ReactNode>(null);
-  const [, startTransition] = useTransition();
-
-  useEffect(() => {
-    const id = window.setTimeout(() => {
-      startTransition(() => {
-        setContent(parse(html, parserOptions));
-      });
-    }, 0);
-
-    return () => window.clearTimeout(id);
-  }, [html]);
-
   return (
     <>
-      {content}
+      {parse(html, parserOptions)}
       <About />
     </>
   );
