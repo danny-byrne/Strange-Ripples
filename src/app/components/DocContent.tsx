c1b08b36b96a71c1f07dd5e168b42a98952ef0f0
danny-byrne <danny.byrne.dev@gmail.com>
2026-03-05 19:35:52 -0800

    try new DocContent

--- src/app/components/DocContent.tsx
+++ src/app/components/DocContent.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import parse from "html-react-parser";
+import { useMemo } from "react";
 import { parserOptions } from "./utils";
 import About from "./About";
 
 export default function DocContent({ html }: { html: string }) {
+  const content = useMemo(() => parse(html, parserOptions), [html]);
+
   return (
     <>
-      {parse(html, parserOptions)}
-      <About />
+      {content}
+      {content ? <About /> : null}
     </>
   );
 }
