c8594b7c15a3660201e51a3674948e8e2062e55e
danny-byrne <danny.byrne.dev@gmail.com>
2026-03-05 20:12:11 -0800

    delay About render

--- src/app/components/DocContent.tsx
+++ src/app/components/DocContent.tsx
@@ -1,17 +1,26 @@
 "use client";
 
 import parse from "html-react-parser";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { parserOptions } from "./utils";
 import About from "./About";
 
 export default function DocContent({ html }: { html: string }) {
   const content = useMemo(() => parse(html, parserOptions), [html]);
+  const [showAbout, setShowAbout] = useState(false);
+
+  useEffect(() => {
+    const id = window.setTimeout(() => {
+      setShowAbout(true);
+    }, 1200);
+
+    return () => window.clearTimeout(id);
+  }, []);
 
   return (
     <>
       {content}
-      {/* {content ? <About /> : null} */}
+      {showAbout ? <About /> : null}
     </>
   );
 }
