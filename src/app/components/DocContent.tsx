a8236804d71dac9b74bca80096cfcfbf111f575a
danny-byrne <danny.byrne.dev@gmail.com>
2026-03-05 18:07:39 -0800

    add setTransition DocContent.tsx

--- src/app/components/DocContent.tsx
+++ src/app/components/DocContent.tsx
@@ -1,13 +1,27 @@
 "use client";
 
 import parse from "html-react-parser";
+import { useEffect, useState, useTransition } from "react";
 import { parserOptions } from "./utils";
 import About from "./About";
 
 export default function DocContent({ html }: { html: string }) {
+  const [content, setContent] = useState<React.ReactNode>(null);
+  const [, startTransition] = useTransition();
+
+  useEffect(() => {
+    const id = window.setTimeout(() => {
+      startTransition(() => {
+        setContent(parse(html, parserOptions));
+      });
+    }, 0);
+
+    return () => window.clearTimeout(id);
+  }, [html]);
+
   return (
     <>
-      {parse(html, parserOptions)}
+      {content}
       <About />
     </>
   );
