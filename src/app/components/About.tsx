535a6ce8ae37b15dc69d781afcf11328039749dd
danny-byrne <danny.byrne.dev@gmail.com>
2026-03-07 23:16:53 -0800

    commit

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -23,8 +23,8 @@ const About: React.FC = () => {
       <div className={styles.bioContainer}>
         <h2>About the Author</h2>
         <div className={styles.text}>
-          Danny Byrne is a software engineer and musician. He is currently
-          writing a book about his experiences facilitating groups with
+          Danny Byrne is a software engineer and musician. This is the first
+          chapter of a book about his experiences facilitating groups with
           Ayahuasca and Psilocybin mushrooms. He creates meditative ceremony
           music as <a href="https://harmala.bandcamp.com">Harmala</a>, and the
           co-founder of{" "}
